04de9047b0e77b7f64d8083b774bf56b3d757bde
AppleClient <appu26j@gmail.com>
2023-08-20 19:10:29 -0700

    fix

--- eclipse/src/appu26j/utils/LauncherDetector.java
+++ eclipse/src/appu26j/utils/LauncherDetector.java
@@ -6,6 +6,8 @@ public class LauncherDetector
 {
     private static final File minecraftLauncherWindows = new File(System.getenv("APPDATA"), ".minecraft");
     private static final File minecraftLauncherMacOS = new File(System.getProperty("user.home") + "/Library/Application Support", "minecraft");
+    private static final File tlLegacyLauncherWindows = new File(System.getenv("APPDATA"), ".tlauncher" + File.separator + "legacy" + File.separator + "Minecraft" + File.separator + "game");
+    private static final File tlLegacyLauncherMacOS = new File(System.getProperty("user.home") + "/Library/Application Support", "tlauncher");
     private static final File minecraftLauncherWindowsCheck = new File(minecraftLauncherWindows, "launcher_profiles.json");
     private static final File minecraftLauncherMacOSCheck = new File(minecraftLauncherMacOS, "launcher_profiles.json");
 
@@ -16,6 +18,6 @@ public class LauncherDetector
 
     public static File getMinecraftLauncherPath()
     {
-        return OSUtil.isOnWindows() ? minecraftLauncherWindows : minecraftLauncherMacOS;
+        return OSUtil.isOnWindows() ? minecraftLauncherWindows.exists() ? minecraftLauncherWindows : tlLegacyLauncherWindows : minecraftLauncherMacOS.exists() ? minecraftLauncherMacOS : tlLegacyLauncherMacOS;
     }
 }
